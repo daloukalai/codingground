7a43969208d8e2980c817daa313ff303899bead9
daloukalai <daloukalai@gmail.com>
2016-11-29 12:33:26 +0530

    Created by Coding Ground Client

---
+++ New Project-20161129/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void Proasnth()
+{
+    cout << "Hi Prashanth" << endl;
+}
+
+int main()
+{
+   cout << "Hello World" << endl; 
+   Proasnth();
+   return 0;
+}
+
